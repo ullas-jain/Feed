88dba55a598328c073a620dbf712ab22a4297b23
ullas-jain <jainullas1994@gmail.com>
2023-08-19 12:09:36 +0530

    Delivers empty items array on 200 HTTP response with JSON empty list

--- EssentialFeed/RemoteFeedLoader.swift
+++ EssentialFeed/RemoteFeedLoader.swift
@@ -26,7 +26,7 @@ public final class RemoteFeedLoader {
     }
     
     public enum Result: Equatable {
-        case success
+        case success([FeedItem])
         case failure(Error)
     }
     
@@ -38,8 +38,12 @@ public final class RemoteFeedLoader {
     public func load(completion: @escaping (Result) -> Void) {
         client.get(from: url) { result in
             switch result {
-            case .success:
-                completion(.failure(.invalidData))
+            case let .success(data, _):
+                if let _ = try? JSONSerialization.jsonObject(with: data) {
+                    completion(.success([]))
+                } else {
+                    completion(.failure(.invalidData))
+                }
             case .failure:
                 completion(.failure(.connectivity))
             }
