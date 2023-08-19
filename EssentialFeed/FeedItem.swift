a1054999dbbb9f0ca7469142ebd75768042ed721
ullas-jain <jainullas1994@gmail.com>
2023-08-19 09:21:09 +0530

    Creates `FeedItem` model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Jain Ullas on 8/19/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
