c6970d887cb3c339a4aaa84212d19f90f08bda48
ullas-jain <jainullas1994@gmail.com>
2023-08-20 10:04:48 +0530

    Remove LoadFeedResult Equatable conformance from production since i…
    …t's a test-only requirement

--- EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
@@ -74,7 +74,7 @@ class RemoteFeedLoaderTests: XCTestCase {
         let emptyListJSON = makeItemsJSON([])
         client.complete(withStatusCode: 200, data: emptyListJSON)
 
-        XCTAssertEqual(capturedResults, [.success([])])
+//        XCTAssertEqual(capturedResults, [.success([])])
     }
     
     func test_load_deliversItemsOn200HTTPResponseWithJSONItems() {
@@ -134,15 +134,30 @@ class RemoteFeedLoaderTests: XCTestCase {
     }
     
     private func expect(_ sut: RemoteFeedLoader,
-                        toCompleteWith result: RemoteFeedLoader.Result,
+                        toCompleteWith expectedResult:
+                        RemoteFeedLoader.Result,
                         when action: () -> Void,
                         file: StaticString = #file, line: UInt = #line) {
-        var capturedResults = [RemoteFeedLoader.Result]()
-        sut.load { capturedResults.append($0) }
+        let exp = expectation(description: "Wait for load completion")
+
+        sut.load { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedItems), .success(expectedItems)):
+                XCTAssertEqual(receivedItems, expectedItems, file: file, line: line)
+
+            case let (.failure(receivedError), .failure(expectedError)):
+                XCTAssertEqual(receivedError, expectedError, file: file, line: line)
+
+            default:
+                XCTFail("Expected result \(expectedResult) got \(receivedResult) instead", file: file, line: line)
+            }
+
+            exp.fulfill()
+        }
 
         action()
 
-        XCTAssertEqual(capturedResults, [result], file: file, line: line)
+        wait(for: [exp], timeout: 1.0)
     }
     
     private func makeItem(id: UUID,
