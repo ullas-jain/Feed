e8a55e411e47aee831b685e82694d2d4d4fb3d1f
ullas-jain <jainullas1994@gmail.com>
2023-08-25 11:16:28 +0530

    Adds a new target `EssentialFeedAPIEndToEndTests` for integration testing of API module

---
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -0,0 +1,16 @@
+//
+//  EssentialFeedAPIEndToEndTests.swift
+//  EssentialFeedAPIEndToEndTests
+//
+//  Created by Jain Ullas on 8/25/23.
+//
+
+import XCTest
+
+final class EssentialFeedAPIEndToEndTests: XCTestCase {
+
+    func test_example() {
+        XCTAssertEqual(2 + 2, 4)
+    }
+
+}
