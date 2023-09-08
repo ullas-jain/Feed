573605aba0d55a31dd840e74b673b8003adb2a7b
ullas-jain <jainullas1994@gmail.com>
2023-09-08 11:47:41 +0530

    Clean up & undo all cache side-effects on setUp and tearDown to a…
    …void sharing state between tests

--- EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -9,42 +9,65 @@ import EssentialFeed
 import XCTest
 
 final class EssentialFeedCacheIntegrationTests: XCTestCase {
-    
+    override func setUp() {
+        super.setUp()
+
+        setupEmptyStoreState()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+
+        undoStoreSideEffects()
+    }
+
     func test_load_deliversNoItemsOnEmptyCache() {
-            let sut = makeSUT()
+        let sut = makeSUT()
 
-            let exp = expectation(description: "Wait for load completion")
-            sut.load { result in
-                switch result {
-                case let .success(imageFeed):
-                    XCTAssertEqual(imageFeed, [], "Expected empty feed")
+        let exp = expectation(description: "Wait for load completion")
+        sut.load { result in
+            switch result {
+            case let .success(imageFeed):
+                XCTAssertEqual(imageFeed, [], "Expected empty feed")
 
-                case let .failure(error):
-                    XCTFail("Expected successful feed result, got \(error) instead")
-                }
-
-                exp.fulfill()
+            case let .failure(error):
+                XCTFail("Expected successful feed result, got \(error) instead")
             }
-            wait(for: [exp], timeout: 1.0)
-        }
 
-        // MARK: Helpers
-        
-        private func makeSUT(file: StaticString = #file, line: UInt = #line) -> LocalFeedLoader {
-            let storeBundle = Bundle(for: CoreDataFeedStore.self)
-            let storeURL = testSpecificStoreURL()
-            let store = try! CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
-            let sut = LocalFeedLoader(store: store, currentDate: Date.init)
-            trackForMemoryLeaks(store, file: file, line: line)
-            trackForMemoryLeaks(sut, file: file, line: line)
-            return sut
+            exp.fulfill()
         }
+        wait(for: [exp], timeout: 1.0)
+    }
 
-        private func testSpecificStoreURL() -> URL {
-            return cachesDirectory().appendingPathComponent("\(type(of: self)).store")
-        }
+    // MARK: Helpers
 
-        private func cachesDirectory() -> URL {
-            return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
-        }
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> LocalFeedLoader {
+        let storeBundle = Bundle(for: CoreDataFeedStore.self)
+        let storeURL = testSpecificStoreURL()
+        let store = try! CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
+        let sut = LocalFeedLoader(store: store, currentDate: Date.init)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
+
+    private func testSpecificStoreURL() -> URL {
+        cachesDirectory().appendingPathComponent("\(type(of: self)).store")
+    }
+
+    private func cachesDirectory() -> URL {
+        FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+    }
+
+    private func setupEmptyStoreState() {
+        deleteStoreArtifacts()
+    }
+
+    private func undoStoreSideEffects() {
+        deleteStoreArtifacts()
+    }
+
+    private func deleteStoreArtifacts() {
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
+    }
 }
