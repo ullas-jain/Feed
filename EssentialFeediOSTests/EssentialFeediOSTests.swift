39152902908499b6dc761c256e71507ebceac210
ullas-jain <jainullas1994@gmail.com>
2023-09-11 11:13:40 +0530

    Add EssentialFeediOS framework (prod and test) targets for iOS plat…
    …form-specific components

---
+++ EssentialFeediOSTests/EssentialFeediOSTests.swift
@@ -0,0 +1,11 @@
+//
+//  EssentialFeediOSTests.swift
+//  EssentialFeediOSTests
+//
+//  Created by Jain Ullas on 9/11/23.
+//
+
+import EssentialFeediOS
+import XCTest
+
+final class EssentialFeediOSTests: XCTestCase {}
