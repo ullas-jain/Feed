ed1260dc423ffd51e2a83ddcf14074bc6ed1639b
ullas.jain <ullas.jain.github@gmail.com>
2023-10-05 20:52:38 +0530

    Move ViewModels to separate files

---
+++ EssentialFeediOS/Feed UI/Presenters/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by ULLAS JAIN on 05/10/23.
+//
+
+import Foundation
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
