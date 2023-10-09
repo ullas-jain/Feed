7c0f7321722d987952dcd45b47bec0635f9647a6
ullas.jain <ullas.jain.github@gmail.com>
2023-10-09 11:24:18 +0530

    Move ErrorView to separate file

---
+++ EssentialFeediOS/Feed UI/Views/FeedErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  FeedErrorView.swift
+//  EssentialFeediOS
+//
+//  Created by ULLAS JAIN on 09/10/23.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
