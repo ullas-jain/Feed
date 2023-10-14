d6ed591a6ad11c085963799068903d98ea0ebe53
ullas.jain <ullas.jain.github@gmail.com>
2023-10-14 16:11:03 +0530

    FeedImagePresenter displays loading image on

--- EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
+++ EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -5,10 +5,41 @@
 //  Created by ULLAS JAIN on 14/10/23.
 //
 
+import EssentialFeed
 import XCTest
 
+struct FeedImageViewModel {
+    let description: String?
+    let location: String?
+    let image: Any?
+    let isLoading: Bool
+    let shouldRetry: Bool
+
+    var hasLocation: Bool {
+        location != nil
+    }
+}
+
+protocol FeedImageView {
+    func display(_ model: FeedImageViewModel)
+}
+
 class FeedImagePresenter {
-    init(view _: Any) {}
+    private let view: FeedImageView
+
+    init(view: FeedImageView) {
+        self.view = view
+    }
+
+    func didStartLoadingImageData(for model: FeedImage) {
+        view.display(FeedImageViewModel(
+            description: model.description,
+            location: model.location,
+            image: nil,
+            isLoading: true,
+            shouldRetry: false
+        ))
+    }
 }
 
 class FeedImagePresenterTests: XCTestCase {
@@ -16,6 +47,21 @@ class FeedImagePresenterTests: XCTestCase {
         let (_, view) = makeSUT()
 
         XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
+    }
+
+    func test_didStartLoadingImageData_displaysLoadingImage() {
+        let (sut, view) = makeSUT()
+        let image = uniqueImage()
+
+        sut.didStartLoadingImageData(for: image)
+
+        let message = view.messages.first
+        XCTAssertEqual(view.messages.count, 1)
+        XCTAssertEqual(message?.description, image.description)
+        XCTAssertEqual(message?.location, image.location)
+        XCTAssertEqual(message?.isLoading, true)
+        XCTAssertEqual(message?.shouldRetry, false)
+        XCTAssertNil(message?.image)
     }
 
     // MARK: - Helpers
@@ -28,7 +74,11 @@ class FeedImagePresenterTests: XCTestCase {
         return (sut, view)
     }
 
-    private class ViewSpy {
-        let messages = [Any]()
+    private class ViewSpy: FeedImageView {
+        private(set) var messages = [FeedImageViewModel]()
+
+        func display(_ model: FeedImageViewModel) {
+            messages.append(model)
+        }
     }
 }
