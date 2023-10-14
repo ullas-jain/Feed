94dec277f2426371477cca55884467943aa20581
ullas.jain <ullas.jain.github@gmail.com>
2023-10-14 15:50:36 +0530

    Move viewmodels to separate files

---
+++ EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by ULLAS JAIN on 14/10/23.
+//
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
---
+++ EssentialFeed/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by ULLAS JAIN on 14/10/23.
+//
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
