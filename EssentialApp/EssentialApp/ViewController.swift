d640cc15dd103fb231ac33051aa0e4a4e251dbd5
ullas.jain <ullas.jain.github@gmail.com>
2023-10-25 14:06:02 +0530

    Add empty essential app project

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by ULLAS JAIN on 25/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
