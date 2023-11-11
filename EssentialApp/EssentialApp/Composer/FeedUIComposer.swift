17ea204f5dee38709dc27a195004aba2ab623012
ullas.jain <ullas.jain.github@gmail.com>
2023-11-11 11:45:28 +0530

    Replace FeedImageDataLoader composition with Combine operators

--- EssentialApp/EssentialApp/Composer/FeedUIComposer.swift
+++ EssentialApp/EssentialApp/Composer/FeedUIComposer.swift
@@ -12,9 +12,10 @@ import UIKit
 public final class FeedUIComposer {
     private init() {}
 
-    public static func feedComposedWith(feedLoader: @escaping () -> FeedLoader.Publisher, imageLoader: FeedImageDataLoader) -> FeedViewController {
+    public static func feedComposedWith(feedLoader: @escaping () -> FeedLoader.Publisher, imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher) -> FeedViewController {
         let presentationAdapter = FeedLoaderPresentationAdapter(
             feedLoader: { feedLoader().dispatchOnMainQueue() })
+
         let feedController = makeFeedViewController(
             delegate: presentationAdapter,
             title: FeedPresenter.title
@@ -23,11 +24,12 @@ public final class FeedUIComposer {
         presentationAdapter.presenter = FeedPresenter(
             feedView: FeedViewAdapter(
                 controller: feedController,
-                imageLoader: MainQueueDispatchDecorator(decoratee: imageLoader)
+                imageLoader: { imageLoader($0).dispatchOnMainQueue() }
             ),
             loadingView: WeakRefVirtualProxy(feedController),
             errorView: WeakRefVirtualProxy(feedController)
         )
+
         return feedController
     }
 
