47c3a0e6e3421ca3d0fc5e66ae6e764654448791
ullas.jain <ullas.jain.github@gmail.com>
2023-11-21 10:41:03 +0530

    Rename and move ResourceLoadingView and VIewModel to Shared module

---
+++ EssentialFeed/Shared Presentation/ResourceLoadingView.swift
@@ -0,0 +1,12 @@
+//
+//  ResourceLoadingView.swift
+//  EssentialFeed
+//
+//  Created by ULLAS JAIN on 21/11/23.
+//
+
+import Foundation
+
+public protocol ResourceLoadingView {
+    func display(_ viewModel: ResourceLoadingViewModel)
+}
