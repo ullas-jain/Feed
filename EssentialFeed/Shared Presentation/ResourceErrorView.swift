291bb4d23168ebef304d36478039a27760fc6ce8
ullas.jain <ullas.jain.github@gmail.com>
2023-11-21 10:42:29 +0530

    Rename and move ResourceErrorView and ViewModel to Shared module

---
+++ EssentialFeed/Shared Presentation/ResourceErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ResourceErrorView.swift
+//  EssentialFeed
+//
+//  Created by ULLAS JAIN on 21/11/23.
+//
+
+import Foundation
+
+public protocol ResourceErrorView {
+    func display(_ viewModel: ResourceErrorViewModel)
+}
--- EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
+++ EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -67,7 +67,7 @@ class FeedPresenterTests: XCTestCase {
         return (sut, view)
     }
 
-    private class ViewSpy: FeedView, FeedErrorView, ResourceLoadingView {
+    private class ViewSpy: FeedView, ResourceErrorView, ResourceLoadingView {
         enum Message: Hashable {
             case display(errorMessage: String?)
             case display(isLoading: Bool)
@@ -76,7 +76,7 @@ class FeedPresenterTests: XCTestCase {
 
         private(set) var messages = Set<Message>()
 
-        func display(_ viewModel: FeedErrorViewModel) {
+        func display(_ viewModel: ResourceErrorViewModel) {
             messages.insert(.display(errorMessage: viewModel.message))
         }
 
