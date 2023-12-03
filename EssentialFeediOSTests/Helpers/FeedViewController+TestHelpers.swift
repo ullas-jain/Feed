a02b6e3e9bdd9949d15c18ee1b77d947e2c6d131
ullas.jain <ullas.jain.github@gmail.com>
2023-12-03 08:22:29 +0530

    Use diffable data source to only reload cells when needed

--- EssentialFeediOSTests/Helpers/FeedViewController+TestHelpers.swift
+++ EssentialFeediOSTests/Helpers/FeedViewController+TestHelpers.swift
@@ -9,6 +9,12 @@ import EssentialFeediOS
 import UIKit
 
 extension ListViewController {
+    override public func loadViewIfNeeded() {
+        super.loadViewIfNeeded()
+
+        tableView.frame = CGRect(x: 0, y: 0, width: 1, height: 1)
+    }
+
     func simulateUserInitiatedFeedReload() {
         refreshControl?.simulatePullToRefresh()
     }
@@ -55,7 +61,7 @@ extension ListViewController {
     }
 
     func numberOfRenderedFeedImageViews() -> Int {
-        tableView.numberOfRows(inSection: feedImagesSection)
+        tableView.numberOfSections == 0 ? 0 : tableView.numberOfRows(inSection: feedImagesSection)
     }
 
     func feedImageView(at row: Int) -> UITableViewCell? {
