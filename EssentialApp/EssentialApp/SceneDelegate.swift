ab87add09600b5b1e5520c6276c80329b96d7a3d
ullas.jain <ullas.jain.github@gmail.com>
2023-12-11 13:06:43 +0530

    Update Feed API URL

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -21,8 +21,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     )
 
     private lazy var remoteFeedLoader = {
-        let url = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential_app_feed.json")!
-        return httpClient.getPublisher(url: url)
+        let remoteURL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
+        return httpClient.getPublisher(url: remoteURL)
     }()
 
     private lazy var localFeedLoader: LocalFeedLoader = .init(store: store, currentDate: Date.init)
