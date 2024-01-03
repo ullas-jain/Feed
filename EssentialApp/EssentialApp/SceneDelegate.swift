c89b8662629b9ef3c05b233144cfcab797b56db9
ullas.jain <ullas.jain.github@gmail.com>
2024-01-03 12:25:04 +0530

    Log fault when we cant create a CoreDataFeedStore instance

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -7,12 +7,14 @@
 import Combine
 import CoreData
 import EssentialFeed
+import os
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     private lazy var httpClient: HTTPClient = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+    private lazy var logger = Logger(subsystem: "com.essentialdeveloper.EssentialAppCaseStudy", category: "main")
 
     private lazy var store: FeedStore & FeedImageDataStore = {
         do {
@@ -22,6 +24,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                     .appendingPathComponent("feed-store.sqlite"))
         } catch {
             assertionFailure("Failed to instantiate CoreData store with error: \(error.localizedDescription)")
+            logger.fault("Failed to instantiate CoreData store with error: \(error.localizedDescription)")
             return NullStore()
         }
     }()
