8daa1cea382359ab09a9ef1b3f0e0f7745ef0aca
ullas.jain <ullas.jain.github@gmail.com>
2024-01-11 20:34:39 +0530

    Rename method to clarify intent

--- EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
@@ -8,43 +8,43 @@
 import CoreData
 
 public final class CoreDataFeedStore {
-	private static let modelName = "FeedStore"
-	private static let model = NSManagedObjectModel.with(name: modelName, in: Bundle(for: CoreDataFeedStore.self))
-	
-	private let container: NSPersistentContainer
-	private let context: NSManagedObjectContext
+    private static let modelName = "FeedStore"
+    private static let model = NSManagedObjectModel.with(name: modelName, in: Bundle(for: CoreDataFeedStore.self))
 
-	enum StoreError: Error {
-		case modelNotFound
-		case failedToLoadPersistentContainer(Error)
-	}
+    private let container: NSPersistentContainer
+    private let context: NSManagedObjectContext
 
-	public init(storeURL: URL) throws {
-		guard let model = CoreDataFeedStore.model else {
-			throw StoreError.modelNotFound
-		}
-		
-		do {
-			container = try NSPersistentContainer.load(name: CoreDataFeedStore.modelName, model: model, url: storeURL)
-			context = container.newBackgroundContext()
-		} catch {
-			throw StoreError.failedToLoadPersistentContainer(error)
-		}
-	}
+    enum StoreError: Error {
+        case modelNotFound
+        case failedToLoadPersistentContainer(Error)
+    }
 
-	func perform(_ action: @escaping (NSManagedObjectContext) -> Void) {
-		let context = self.context
-		context.perform { action(context) }
-	}
-	
-	private func cleanUpReferencesToPersistentStores() {
-		context.performAndWait {
-			let coordinator = self.container.persistentStoreCoordinator
-			try? coordinator.persistentStores.forEach(coordinator.remove)
-		}
-	}
-	
-	deinit {
-		cleanUpReferencesToPersistentStores()
-	}
+    public init(storeURL: URL) throws {
+        guard let model = CoreDataFeedStore.model else {
+            throw StoreError.modelNotFound
+        }
+
+        do {
+            container = try NSPersistentContainer.load(name: CoreDataFeedStore.modelName, model: model, url: storeURL)
+            context = container.newBackgroundContext()
+        } catch {
+            throw StoreError.failedToLoadPersistentContainer(error)
+        }
+    }
+
+    func performAsync(_ action: @escaping (NSManagedObjectContext) -> Void) {
+        let context = self.context
+        context.perform { action(context) }
+    }
+
+    private func cleanUpReferencesToPersistentStores() {
+        context.performAndWait {
+            let coordinator = self.container.persistentStoreCoordinator
+            try? coordinator.persistentStores.forEach(coordinator.remove)
+        }
+    }
+
+    deinit {
+        cleanUpReferencesToPersistentStores()
+    }
 }
